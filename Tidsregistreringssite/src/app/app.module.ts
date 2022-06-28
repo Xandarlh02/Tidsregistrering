63f9a80570185754b92a48884566c6597273a652
Xandarlh02 <xandarlh@gmail.com>
2022-06-28 20:40:54 +0200

    d

--- Tidsregistreringssite/src/app/app.module.ts
+++ Tidsregistreringssite/src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//Pages
 import { InputPageComponent } from './input-page/input-page.component';
+import { OverviewPageComponent } from './overview-page/overview-page.component';
 
 //Material
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { OverviewPageComponent } from './overview-page/overview-page.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [
@@ -20,18 +27,28 @@ import { OverviewPageComponent } from './overview-page/overview-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
 
     //Material
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- Tidsregistreringssite/src/app/input-page/input-page.component.ts
+++ Tidsregistreringssite/src/app/input-page/input-page.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-input-page',
   templateUrl: './input-page.component.html',
   styleUrls: ['./input-page.component.css']
 })
+
 export class InputPageComponent implements OnInit {
 
-  constructor() { }
 
+  name: string = '';
+  date = ""
+
+  constructor() { }
+  
   ngOnInit(): void {
   }
+  
 
 }
